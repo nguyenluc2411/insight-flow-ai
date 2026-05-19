5e4e19f481d7952ea1760fc0bc088ddca929d877
nguyenluc2411 <lucpro2004sn@gmail.com>
2026-05-20 01:17:08 +0700

    feat(integration): phase I1 - project skeleton

---
+++ integration-services/integration-service/src/test/java/com/insightflow/integration/IntegrationServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.insightflow.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class IntegrationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
